1e03bcbf27061d20cb9a6ce4cb166e648cc3d82b
ldselvera <luiselvera97@gmail.com>
2020-03-28 22:28:47 -0500

    website v1.0

--- deploy.py
+++ deploy.py
@@ -13,19 +13,20 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return '<h1> deploy </h1> <h3> this is </h3>'
+  #return render_template('index.html')
 
-@app.route('/classification', methods=['POST', 'GET'])
-def result():
-  if request.method == 'POST':
-    question = request.form['text']
+# @app.route('/classification', methods=['POST', 'GET'])
+# def result():
+#   if request.method == 'POST':
+#     question = request.form['text']
   
-  if question=='':
-    answer="Please provide a comment."
-  else:
-    answer='Not a troll.'
-    #answer = prediction(question, model, tokenizer)
-  return render_template("classification.html", result= answer)
+#   if question=='':
+#     answer="Please provide a comment."
+#   else:
+#     answer='Not a troll.'
+#     #answer = prediction(question, model, tokenizer)
+#   return render_template("classification.html", result= answer)
 
 # if __name__ == '__main__':
 #   app.run()
