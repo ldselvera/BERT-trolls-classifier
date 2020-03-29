12f0307587f9129054ead77d04a6785744a4bf1a
ldselvera <luiselvera97@gmail.com>
2020-03-28 21:59:22 -0500

    website v1.0

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, session, redirect
+# import os
+# import sys
+# import numpy as np
+# import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# from model.predict import prediction
+# from model.load import load_info
+
+app = Flask(__name__)
+
+# global model
+# model, tokenizer = load_info()
+ 
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/classification', methods=['POST', 'GET'])
+def result():
+  if request.method == 'POST':
+    question = request.form['text']
+  
+  if question=='':
+    answer="Please provide a comment."
+  else:
+    answer='Not a troll.'
+    #answer = prediction(question, model, tokenizer)
+  return render_template("classification.html", result= answer)
+
+if __name__ == '__main__':
+  app.run()
